a402059afaeb050c6d96c5985e989d09525813fd
Aleksandar Milisavljevic <aleksa1302@gmail.com>
2018-03-14 16:35:32 -0500

    Initial commit

---
+++ www/js/config.js
@@ -0,0 +1,14 @@
+'use strict';
+app
+
+.constant('config', { 
+
+  urlBase: 'YOUR_BACKEND_URL',
+  currency: '$',
+  onesignalId: 'YOUR_ONESIGNAL_ID',
+  playstoreAppId: 'YOUR_PLAYSTORE_APP_ID',
+  appstoreAppId: 'YOUR_APPSTORE_ID',
+  playstoreAppUrl: 'https://play.google.com/store/apps/details?id=',
+  appstoreAppUrl: 'https://itunes.apple.com/in/app/',
+
+});
